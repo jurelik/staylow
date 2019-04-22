48e8207fc48597766a5c86f042ecc40abd2bf658
jurelik <jure@likar.me>
2019-04-22 18:25:51 +0100

    added mute for password fields

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const readline = require('readline');
 
 let buffer = []; //Buffer of pressed keys
 let promptActive = false; //Is a prompt active?
+let muted = false; //Is mute active?
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -28,24 +29,62 @@ process.stdin.on('keypress', (val, key) => {
   }
 });
 
+//Change the readline logging function
+rl._writeToOutput = function _writeToOutput(stringToWrite) {
+  if (muted === true) {
+    rl.output.write("*");
+  }
+  else {
+    rl.output.write(stringToWrite);
+  }
+};
+
 //Custom log function
 exports.log = function(text) {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
   console.log(text);
-  process.stdout.write(buffer.join(''));
+  if (muted === true) {
+    process.stdout.write(buffer[0]);
+    for (x = 1; x < buffer.length; x++) {
+      process.stdout.write('*');
+    }
+  }
+  else {
+    process.stdout.write(buffer.join(''));
+  }
 };
 
 //Custom prompt function
-exports.prompt = function(q, callback) {
-  if (q === '') {
-    q = '> ';
+exports.prompt = function(question, mute, callback) {
+
+  //Change the readline question function
+  rl.question = function(query, cb) {
+    if (typeof cb === 'function') {
+      if (rl._questionCallback) {
+        rl.prompt();
+      } else {
+        rl._oldPrompt = this._prompt;
+        rl.setPrompt(query);
+        rl._questionCallback = cb;
+        rl.prompt();
+        //Inject this bit
+        if (mute) {
+          muted = true;
+        }
+        //
+      }
+    }
+  }; 
+  if (question === '') {
+    question = '> ';
   }
-  buffer.unshift(q);
-  rl.question(q, (res) => {
+  buffer.unshift(question);
+  rl.question(question, (res) => {
     promptActive = true;
     buffer = [];
+    muted = false;
     callback(res);
   });
-};
+}
 
